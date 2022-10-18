46eba95a5c28facd3fa01acef2a51af6f751b12c
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-10-17 20:01:00 -0400

    add more fields to product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 export interface ProductProps {
   id: number;
   description: string;
-  //codbarras: number;
+  codbarras: number;
 }
 
 @Entity({ name: 'PCPRODUT', synchronize: false })
@@ -14,4 +14,16 @@ export class Product implements ProductProps {
   description: string;
   @Column({ name: 'CODAUXILIAR' })
   codbarras: number;
+  @Column({ name: 'DTCADASTRO', type: 'date' })
+  createdAt: string;
+  @Column({ name: 'DTEXCLUSAO' })
+  excludedAt: string;
+  @Column({ name: 'DTULTALTER' })
+  lastModification: string;
+  @Column({ name: 'CODEPTO' })
+  idDep: number;
+  @Column({ name: 'CODSEC' })
+  idSec: number;
+  @Column({ name: 'CODCATEGORIA' })
+  idCat: number;
 }
